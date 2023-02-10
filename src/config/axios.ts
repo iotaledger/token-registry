a51003084119aa5eef8260cbf1f633ec5036aeb8
Mario Sarcevic <mario.sarcevic@iota.org>
2023-02-10 15:29:19 +0100

    Remove need for github token for github API read

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -5,16 +5,10 @@ if (!githubApiRootPath) {
     throw new Error("Github api root path not set.");
 }
 
-const githubApiToken = process.env.GITHUB_TOKEN;
-if (!githubApiToken) {
-    throw new Error("Github api token not set.");
-}
-
 export const githubApiClient = axios.create({
     baseURL: githubApiRootPath,
     headers: {
-        "Accept": "application/vnd.github+json",
-        "Authorization": `Bearer ${githubApiToken}`
+        "Accept": "application/vnd.github+json"
     }
 });
 
